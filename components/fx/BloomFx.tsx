1905dcae2485abc49333992b8ea272cfc8f6cb6c
Saptarshi2006 <saptarshimukherjee2024@gmail.com>
2026-08-20 22:14:56 +0530

    feat: vignette + film grain + chromatic aberration on shader worlds

--- components/fx/BloomFx.tsx
+++ components/fx/BloomFx.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { EffectComposer, Bloom } from "@react-three/postprocessing";
+import { EffectComposer, Bloom, Vignette, Noise, ChromaticAberration } from "@react-three/postprocessing";
+import { BlendFunction } from "postprocessing";
+import * as THREE from "three";
 import { useUI } from "@/lib/store";
 
 export default function BloomFx({
@@ -17,6 +19,9 @@ export default function BloomFx({
   return (
     <EffectComposer enableNormalPass={false}>
       <Bloom intensity={intensity} luminanceThreshold={luminanceThreshold} radius={radius} mipmapBlur />
+      <Vignette eskil={false} offset={0.35} darkness={0.42} blendFunction={BlendFunction.NORMAL} />
+      <Noise opacity={0.035} premultiply blendFunction={BlendFunction.SOFT_LIGHT} />
+      <ChromaticAberration offset={new THREE.Vector2(0.0006, 0.0006)} />
     </EffectComposer>
   );
 }
